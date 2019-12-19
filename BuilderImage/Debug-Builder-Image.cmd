39141e38eae327a4dcceeb2354959172b48b48ea
Chad J. Hamilton <chad.hamilton@nttdata.com>
2019-12-19 14:42:48 -0800

    Updated configuration

---
+++ BuilderImage/Debug-Builder-Image.cmd
@@ -0,0 +1,20 @@
+@echo off
+title Debug the Builder Image
+cls
+
+REM Set the Launch Path
+set LaunchPath=%~dp0
+set LaunchPath=%LaunchPath:~0,-1%
+
+REM Set the OpenShift Project
+set OPENSHIFT_PROJECT=
+
+REM Set the Builder Image
+set BUILDER_IMAGE_NAME=
+
+oc run -i -t builder --image=docker-registry.default.svc:5000/%OPENSHIFT_PROJECT%/%BUILDER_IMAGE_NAME% --command -- bash
+
+REM Script finished
+echo Debug the Builder Image script has completed
+echo.
+pause
